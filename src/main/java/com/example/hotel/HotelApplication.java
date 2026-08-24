e1cc453ec02653e3fd5c87bf9e56c0859331ef03
Vishesh Goswami <124872284+vizpecial@users.noreply.github.com>
2026-08-24 21:49:32 +0530

    Remove automatic browser opening on startup, for render
    Removed the CommandLineRunner bean that opens the browser on startup.

--- src/main/java/com/example/hotel/HotelApplication.java
+++ src/main/java/com/example/hotel/HotelApplication.java
@@ -14,26 +14,4 @@ public class HotelApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HotelApplication.class, args);
 	}
-// This automatically opens the browser on startup
-	@Bean
-	CommandLineRunner openBrowser() {
-		return args -> {
-			try {
-				String url = "http://localhost:8008";
-                String os = System.getProperty("os.name").toLowerCase();
-                
-                if (os.contains("win")) {
-                    // Windows-specific command to force-open default browser
-					Runtime.getRuntime().exec(new String[] {
-						"rundll32", "url.dll,FileProtocolHandler", url
-					});
-                } else if (Desktop.isDesktopSupported()) {
-                    // Mac / Linux
-                    Desktop.getDesktop().browse(new URI(url));
-                }
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		};
-	}
 }
